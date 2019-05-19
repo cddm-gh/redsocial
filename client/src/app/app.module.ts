6571521a1cc1e6c313a30000833d0c9a788b9f98
Carlos <carlosd.dev@gmail.com>
2019-05-19 18:43:06 -0400

    Reorganizando estructura del proyecto

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -3,10 +3,15 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+//Componentes
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
