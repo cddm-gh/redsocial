4ee1ee3794cf891f53a41c5128b7497febf92213
Carlos <carlosd.dev@gmail.com>
2019-05-21 07:48:04 -0400

    Modificación de cabeceras para permitir métodos HTTP

--- api/server/app.js
+++ api/server/app.js
@@ -3,6 +3,20 @@ const bodyParser = require('body-parser');
 
 const app = express();
 
+//Cabeceras http Cors
+app.use(function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'token, Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
+    res.header('Access-Control-Allow-Methods', 'PUT,POST,GET,DELETE,PATCH,OPTIONS');
+    res.header('Allow', 'PUT,POST,GET,DELETE,PATCH,OPTIONS');
+
+    // allow preflight
+    if (req.method === 'OPTIONS') {
+        res.sendStatus(200);
+    } else {
+        next();
+    }
+});
 //Cargar rutas
 const user_routes = require('../routes/user');
 const upload_routes = require('../routes/upload');
@@ -14,15 +28,6 @@ const message_routes = require('../routes/message');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-//Cabeceras http Cors
-app.use(function(req, res, next) {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Headers', 'token, Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
-    res.header('Access-Control-Allow-Methods', 'GET POST OPTIONS PUT DELETE');
-    res.header('Allow', 'GET POST OPTIONS PUT DELETE');
-
-    next();
-});
 
 //Rutas
 app.use('/api', user_routes);
